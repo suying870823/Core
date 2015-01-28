90697a0d16dfb451fad35e5e832e12688a3e5bee
Su Ying <Work@WorkdeMacBook-Pro.local>
2015-01-28 11:27:12 +0800

    del

---
+++ Core/Core/Core.h
@@ -0,0 +1,19 @@
+//
+//  Core.h
+//  Core
+//
+//  Created by Su Ying on 15/1/28.
+//  Copyright (c) 2015年 Simon Sui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Core.
+FOUNDATION_EXPORT double CoreVersionNumber;
+
+//! Project version string for Core.
+FOUNDATION_EXPORT const unsigned char CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Core/PublicHeader.h>
+
+
